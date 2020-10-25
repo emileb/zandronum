0659adf004aaefc43f357bea894b6be8fa45fe30
Emile Belanger <emile.belanger@gmail.com>
2020-10-25 11:05:17 +0000

    Fix mouse cursor for android

--- src/sdl/i_system.cpp
+++ src/sdl/i_system.cpp
@@ -936,6 +936,56 @@ SDL_Cursor *CreateColorCursor(FTexture *cursorpic)
 
 SDL_Surface *cursorSurface = NULL;
 SDL_Rect cursorBlit = {0, 0, 32, 32};
+
+#ifdef __ANDROID__
+bool I_SetCursor(FTexture *cursorpic)
+{
+	static SDL_Cursor *cursor;
+	static SDL_Surface *cursorSurface;
+
+	if (cursorpic != NULL && cursorpic->UseType != FTexture::TEX_Null)
+	{
+		// Must be no larger than 32x32.
+		if (cursorpic->GetWidth() > 32 || cursorpic->GetHeight() > 32)
+		{
+			return false;
+		}
+
+		SDL_ShowCursor(SDL_DISABLE);
+		if (cursorSurface == NULL)
+			cursorSurface = SDL_CreateRGBSurface (0, 32, 32, 32, MAKEARGB(0,255,0,0), MAKEARGB(0,0,255,0), MAKEARGB(0,0,0,255), MAKEARGB(255,0,0,0));
+
+		SDL_LockSurface(cursorSurface);
+		uint8_t buffer[32*32*4];
+		memset(buffer, 0, 32*32*4);
+		FBitmap bmp(buffer, 32*4, 32, 32);
+		cursorpic->CopyTrueColorPixels(&bmp, 0, 0);
+		memcpy(cursorSurface->pixels, bmp.GetPixels(), 32*32*4);
+		SDL_UnlockSurface(cursorSurface);
+
+		if (cursor)
+			SDL_FreeCursor (cursor);
+		cursor = SDL_CreateColorCursor (cursorSurface, 0, 0);
+		SDL_SetCursor (cursor);
+		SDL_ShowCursor(SDL_ENABLE);
+	}
+	else
+	{
+		if (cursor)
+		{
+			SDL_SetCursor (NULL);
+			SDL_FreeCursor (cursor);
+			cursor = NULL;
+		}
+		if (cursorSurface != NULL)
+		{
+			SDL_FreeSurface(cursorSurface);
+			cursorSurface = NULL;
+		}
+	}
+	return true;
+}
+#else
 bool I_SetCursor(FTexture *cursorpic)
 {
 	if (cursorpic != NULL && cursorpic->UseType != FTexture::TEX_Null)
@@ -993,3 +1043,4 @@ bool I_SetCursor(FTexture *cursorpic)
 	}
 	return true;
 }
+#endif
