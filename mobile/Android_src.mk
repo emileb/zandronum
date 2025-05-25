29c1b7d50b81f2050a05e6b181eb4a6545ed6029
Emile Belanger <emile.belanger@gmail.com>
2025-05-25 09:49:50 +0100

    Rebuild servercommands.cpp

--- mobile/Android_src.mk
+++ mobile/Android_src.mk
@@ -347,8 +347,8 @@ GAME_ALL = \
 	g_shared/a_debris.cpp \
 	g_shared/a_decals.cpp \
 	g_shared/a_fastprojectile.cpp \
-	g_shared/a_flags.cpp \
 	g_shared/a_flashfader.cpp \
+	g_shared/a_teamitems.cpp \
 	g_shared/a_fountain.cpp \
 	g_shared/a_hatetarget.cpp \
 	g_shared/a_icon.cpp \
